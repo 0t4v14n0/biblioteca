80061c40401c1eb9b931c13a412deb2cff1ba119
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2023-10-05 16:21:28 -0300

    Update bancodedados.py

--- bancodedados.py
+++ bancodedados.py
@@ -2,12 +2,38 @@ import sqlite3
 
 class ConexaoBancoDeDados:
 
-    def cadastrar_Livro(id,nome,genero,autor,ano):
+    #def pesquisa():
+
+    def cadastrar_Livro(leva):
+
+        id,nome,autor,genero,ano = leva
+        conn = sqlite3.connect('meu_banco_de_dados.db')
+        c = conn.cursor()
+
+        c.execute("INSERT INTO Livro (id,nome,genero,ano,autor) VALUES (?,?,?,?,?)",(id,nome,genero,ano,autor))
+        conn.commit()
+
+        conn.close()
+
+    def cadastrar_Autor(leva):
+
+        nome,ano,bio=leva
+        conn = sqlite3.connect('meu_banco_de_dados.db')
+        c = conn.cursor()
+
+        c.execute("INSERT INTO Autor (nome,datadena,biografia) VALUES (?,?,?)",(nome,ano,bio))
+        conn.commit()
+
+        conn.close()
+
+    def cadastrar_Pessoa(leva):
 
         conn = sqlite3.connect('meu_banco_de_dados.db')
         c = conn.cursor()
 
-        c.execute("INSERT INTO Livro (id,nome,genero,ano,autor) VALUES (?,?,?,?,?)",(id,nome,genero,autor,ano))
+        id,nome,cpf = leva
+
+        c.execute("INSERT INTO Pessoa (nome,cpf,id) VALUES (?,?,?)",(nome,cpf,id))
         conn.commit()
 
         conn.close()
