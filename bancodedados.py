b93091eb8501014bf861bed9373a02aca77cdd29
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2023-10-23 15:01:03 -0300

    Update bancodedados.py

--- bancodedados.py
+++ bancodedados.py
@@ -1,60 +1,100 @@
 import sqlite3
+#from interfacegrafica import 
 
 class ConexaoBancoDeDados:
 
-    def pesquisa():
-        print
+    def pesquisa(leva):
+
+        try:
+            conn = sqlite3.connect('meu_banco_de_dados.db')
+            c = conn.cursor()
+            c.execute("SELECT * FROM Livro")
+            resu = c.fetchall()
+            a = 0
+            for linha in resu:
+                print(linha)
+                for i in range(0,5):
+                    if linha[i] == leva:
+                        gua[a] = linha[i]
+                        a+=1
+                        print("encontrado")
+                    else:
+                        print("não encontrado")
+
+            conn.close()
+
+        except sqlite3.Error as e:
+
+            print(f"Erro de banco de dados: {e}")
 
     def cadastrar_Livro(leva):
 
-        id,nome,autor,genero,ano = leva
-        conn = sqlite3.connect('meu_banco_de_dados.db')
-        c = conn.cursor()
+        try:
 
-        c.execute("INSERT INTO Livro (id,nome,genero,ano,autor) VALUES (?,?,?,?,?)",(id,nome,genero,ano,autor))
-        conn.commit()
+            id,nome,autor,genero,ano = leva
+            conn = sqlite3.connect('meu_banco_de_dados.db')
+            c = conn.cursor()
+            c.execute("INSERT INTO Livro (id,nome,genero,ano,autor) VALUES (?,?,?,?,?)",(id,nome,genero,ano,autor))
+            conn.commit()
+            conn.close()
 
-        conn.close()
+        except sqlite3.Error as e:
+            
+            print(f"Erro de banco de dados: {e}")
 
     def cadastrar_Autor(leva):
 
-        nome,ano,bio=leva
-        conn = sqlite3.connect('meu_banco_de_dados.db')
-        c = conn.cursor()
+        try:
 
-        c.execute("INSERT INTO Autor (nome,datadena,biografia) VALUES (?,?,?)",(nome,ano,bio))
-        conn.commit()
+            nome,ano,bio=leva
+            conn = sqlite3.connect('meu_banco_de_dados.db')
+            c = conn.cursor()
+            c.execute("INSERT INTO Autor (nome,datadena,biografia) VALUES (?,?,?)",(nome,ano,bio))
+            conn.commit()
+            conn.close()
 
-        conn.close()
+        except sqlite3.Error as e:
+            
+            print(f"Erro de banco de dados: {e}")
 
     def cadastrar_Pessoa(leva):
 
-        conn = sqlite3.connect('meu_banco_de_dados.db')
-        c = conn.cursor()
+        try:
+            
+            conn = sqlite3.connect('meu_banco_de_dados.db')
+            c = conn.cursor()
+            id,nome,cpf = leva
+            c.execute("INSERT INTO Pessoa (nome,cpf,id) VALUES (?,?,?)",(nome,cpf,id))
+            conn.commit()
+            conn.close()
 
-        id,nome,cpf = leva
-
-        c.execute("INSERT INTO Pessoa (nome,cpf,id) VALUES (?,?,?)",(nome,cpf,id))
-        conn.commit()
-
-        conn.close()
+        except sqlite3.Error as e:
+            
+            print(f"Erro de banco de dados: {e}")
 
     def pegar_emprestado(leva):
 
         try:
             print("deu")
 
-        except:
-            print("erro")
+        except sqlite3.Error as e:
+            
+            print(f"Erro de banco de dados: {e}")
 
-    def devolver():
+    def devolver(leva):
 
-        conn = sqlite3.connect('meu_banco_de_dados.db')
-        c = conn.cursor()
+        try:
 
-        codl,cpf = leva
+            conn = sqlite3.connect('meu_banco_de_dados.db')
+            c = conn.cursor()
 
-        c.execute("",())
-        conn.commit()
+            codl,cpf = leva
 
-        conn.close()
+            c.execute("",())
+            conn.commit()
+
+            conn.close()
+
+        except sqlite3.Error as e:
+            
+            print(f"Erro de banco de dados: {e}")
